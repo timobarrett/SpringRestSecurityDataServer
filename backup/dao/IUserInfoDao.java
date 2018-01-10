f2cb3af2b4a96089a3e249da070d9f188212d32d
timobarrett <timobarrett@gmail.com>
2018-01-10 16:51:13 -0500

    code cleanup

--- backup/dao/IUserInfoDao.java
+++
@@ -1,9 +0,0 @@
-package fivetwentysix.ware.com.securitytry.dao;
-
-import fivetwentysix.ware.com.securitytry.dao.UserInfo;
-
-//@RepositoryRestResource(collectionResourceRel = "user",path="user")
-public interface IUserInfoDao {//extends PagingAndSortingRepository<UserInfo, String> {
-        UserInfo getActiveUser(String userName);
-     //   void save(UserInfo userInfo);
-}
